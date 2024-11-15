a4efb3670d33dd3a4fe091b5796d595c86f38b9a
jayst-jkst <59138247+jayst-jkst@users.noreply.github.com>
2024-11-15 21:53:50 +0530

    Add files via upload

---
+++ backend/src/config/database.postgres.config.js
@@ -0,0 +1,55 @@
+
+import { Sequelize } from 'sequelize';
+import { UserModel } from '../models/user.model.js';
+import { FoodModel } from '../models/food.model.js';
+import { sample_users } from '../data.js';
+import { sample_foods } from '../data.js';
+import bcrypt from 'bcryptjs';
+const PASSWORD_HASH_SALT_ROUNDS = 10;
+
+const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+  dialect: 'postgres',
+  logging: false,
+});
+
+export const dbconnectpostgress = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    await seedUsers();
+    await seedFoods();
+    console.log('Connected to PostgreSQL successfully---');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function seedUsers() {
+  const usersCount = await UserModel.count();
+  if (usersCount > 0) {
+    console.log('Users seed is already done!');
+    return;
+  }
+
+  for (let user of sample_users) {
+    user.password = await bcrypt.hash(user.password, PASSWORD_HASH_SALT_ROUNDS);
+    await UserModel.create(user);
+  }
+
+  console.log('Users seed is done!');
+}
+
+async function seedFoods() {
+  const foodsCount = await FoodModel.count();
+  if (foodsCount > 0) {
+    console.log('Foods seed is already done!');
+    return;
+  }
+
+  for (const food of sample_foods) {
+    food.imageUrl = `/foods/${food.imageUrl}`;
+    await FoodModel.create(food);
+  }
+
+  console.log('Foods seed is done!');
+}
